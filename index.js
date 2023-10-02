7629e31e4fd21cadde19878a78e9e5fb6d837f2a
shekinah <nyamburamary1212@gmail.com>
2023-10-02 16:00:00 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+function submitData(){
+
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+
+return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        const newId = object.id;
+        // Append the new ID to the DOM or perform any desired action.
+        const idElement = document.createElement("p");
+        idElement.textContent = `New ID: ${newId}`;
+        document.body.appendChild(idElement);
+    })
+
+    .catch(function (error) {
+        // Handle the error and append the error message to the DOM.
+        const errorElement = document.createElement('p');
+        errorElement.textContent = `Error: ${error.message}`;
+        document.body.appendChild(errorElement);
+        // Return the error object for further error handling, if needed.
+        return error;
+    });
+
+}
